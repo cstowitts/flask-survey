a49dd35fc6c18cdb45485c99ddd23da44d4577af
Kate Moser <komoser@gmail.com>
2022-01-27 13:49:12 -0600

    Added some pseudo code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from turtle import end_fill
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
@@ -7,3 +8,23 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get('/')
+def show_start():
+    """display survey title, instructions, and start button"""
+
+
+# @app.post(dkfjnsdfkjnsf)
+#     redirect to next question
+
+# @app.get(/questions/<question_index>)
+#     if question_index == survey.questions.length:
+#         go to end
+#     else:
+#         go to /q/questionsuestion_index+1
+
+
+# @app.get(end)
+#     display end

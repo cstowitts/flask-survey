419ab6bae73f273b205f2205d9dd1d880924b872
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2022-01-27 16:47:07 -0800

    completes survey functionality up to step 6

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ debug = DebugToolbarExtension(app)
 
 responses = []
 
+
 @app.get('/')
 def show_start():
     """display survey title, instructions, and start button"""
@@ -17,21 +18,18 @@ def show_start():
     survey_title = survey.title
     survey_instructions = survey.instructions
 
-    return render_template("survey_start.html", survey_title=survey_title, survey_instructions=survey_instructions)
+    return render_template(
+        "survey_start.html",
+        survey_title=survey_title,
+        survey_instructions=survey_instructions)
 
-
-#   this will happen in the POST request when continue button is pressed
-#     if question_index == survey.questions.length:
-#         go to end
-#     else:
-#         go to /q/questionsuestion_index+1
 
 @app.get('/questions/<int:question_index>')
 def show_question(question_index):
     """display appropriate question, choices, and continue button"""
 
     survey_question = survey.questions[question_index]
-    
+
     return render_template("question.html", question=survey_question)
 
 
@@ -42,15 +40,21 @@ def store_answer():
     answer = request.form["answer"]
     responses.append(answer)
     next_question_index = len(responses)
-    
+
     if len(survey.questions) == len(responses):
         return redirect("/thanks")
     else:
         return redirect(f"/questions/{next_question_index}")
-    
+
+
 @app.get('/thanks')
 def display_thanks():
-    return "thnkx"
+    """empty responses list and render survey completion confirmation"""
+
+    global responses
+    responses = []
+
+    return render_template("completion.html")
 
 # @app.get(end)
 #     display end
